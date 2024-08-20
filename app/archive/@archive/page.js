abf9c2e746d4bdfaa3211f321e737bece06ff211
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-08-20 17:57:06 +0100

    add archive pages

---
+++ app/archive/@archive/page.js
@@ -0,0 +1,7 @@
+export default function ArchivePage() {
+  return (
+    <div id="archive">
+      <h1>Archive</h1>
+    </div>
+  );
+}
---
+++ app/archive/@latest/page.js
@@ -0,0 +1,7 @@
+export default function LatestPage() {
+  return (
+    <div id="latest">
+      <h1>Latest</h1>
+    </div>
+  );
+}
