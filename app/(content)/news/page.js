78709eb601ba3ea373be9d70853d9d280f2f0bb5
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-08-24 08:26:27 +0100

    add client side fetching

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,13 +1,51 @@
-import { DUMMY_NEWS } from '@/dummy-news';
+"use client";
+
 import NewsList from '@/components/news-list';
+import { useEffect, useState } from 'react';
 
 export default function NewsPage() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    async function fetchNews() {
+      setLoading(true);
+      const response = await fetch('http://localhost:8080/news');
+
+      if (!response.ok) {
+        setError('Something went wrong!');
+        setLoading(false);
+        return;
+      }
+      
+      const news = await response.json();
+      setLoading(false);
+      setNews(news);
+      
+    }
+    fetchNews()
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newsContent;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
 
   return (
     <>
       <h1>News Page</h1>
 
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
