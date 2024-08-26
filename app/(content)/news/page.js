f0d57227c5482b377a7f530368f60d7813c97d40
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-08-26 07:24:55 +0100

    add server side fetching from separate backend

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,51 +1,16 @@
-"use client";
 
 import NewsList from '@/components/news-list';
-import { useEffect, useState } from 'react';
 
-export default function NewsPage() {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState();
-  const [news, setNews] = useState();
-
-  useEffect(() => {
-    async function fetchNews() {
-      setLoading(true);
-      const response = await fetch('http://localhost:8080/news');
-
-      if (!response.ok) {
-        setError('Something went wrong!');
-        setLoading(false);
-        return;
-      }
-      
-      const news = await response.json();
-      setLoading(false);
-      setNews(news);
-      
-    }
-    fetchNews()
-  }, []);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
-
-  let newsContent;
-
-  if (news && news.length > 0) {
-    newsContent = <NewsList news={news} />;
-  }
+export default async function NewsPage() {
+  
+  const response = await fetch('http://localhost:8080/news');
+  const news = await response.json();
 
   return (
     <>
       <h1>News Page</h1>
 
-      {newsContent}
+      <NewsList news={news} />
     </>
   );
 }
